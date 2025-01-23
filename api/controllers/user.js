194a8e1599dc0faae0731068488cb1070d23fdff
Pratixa ladumor <banshiladumor@gmail.com>
2025-01-23 12:51:39 +0530

    abcd

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -36,54 +36,40 @@ const dashbord = async (req, res) => {
 
 
 const signup = async (req, res) => {
-    console.log(req.body);
     try {
         const { firstname, lastname, email, password } = req.body;
-        const user = await User.findOne({ email });
-        if (user) {
+
+        if (await User.findOne({ email })) {
             return res.status(400).json({ message: "User already exists" });
         }
+
         const hashPassword = await bcryptjs.hash(password, 10);
-        const createdUser = new User({
-            firstname, lastname, email, password: hashPassword,
-        });
-        await createdUser.save();
-        res.status(200).json({ message: "User created successfully" });
+        const newUser = new User({ firstname, lastname, email, password: hashPassword });
+        await newUser.save();
+
+        res.status(201).json({ message: "User created successfully" });
     } catch (error) {
-        console.log("Error: " + error.message);
-        res.status(400).json({ message: "Internal server error" });
+        console.error("Signup Error:", error.message);
+        res.status(500).json({ message: "Internal server error" });
     }
 };
-
-
-
-
 
 const login = async (req, res) => {
     try {
         const { email, password } = req.body;
-        console.log("Received email: ", email, "Received password: ", password);
         const user = await User.findOne({ email });
 
-        if (!user) {
-            console.log("No user found with this email");
+        if (!user || !(await bcryptjs.compare(password, user.password))) {
             return res.status(400).json({ message: "Invalid username or password" });
         }
 
-        const isMatch = await bcryptjs.compare(password, user.password);
-        if (!isMatch) {
-            console.log("Password mismatch");
-            return res.status(400).json({ message: "Invalid username or password" });
-        }
-
-        res.status(200).json({
-            message: "Login successful",
-        });
+        res.status(200).json({ message: "Login successful" });
     } catch (error) {
-        console.log("Error: " + error.message);
+        console.error("Login Error:", error.message);
         res.status(400).json({ message: "Internal server error" });
     }
 };
+
 
 
 module.exports = {
